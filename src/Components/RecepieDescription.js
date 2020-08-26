157c9c9d17b356012bf7d93d4e13374ca1f69d01
Ellinor Carlstedt <ellinor_carlstedt@msn.com>
2020-08-26 21:28:13 +0200

    minor code improvement for readability

--- src/Components/RecepieDescription.js
+++ src/Components/RecepieDescription.js
@@ -3,8 +3,12 @@ import '../App.css';
 
 function RecepieDescription (props) {
     
-    let description = props.description.length ? props.description.map((row, i) => <p className="recepie-description-row" key={i}>{row}</p>) 
-        : (<p className="recepie-description-missing"> ~ Recept saknas ~ </p>)
+    let description = [];
+    if (props.description.length) {
+        description = props.description.map((row, i) => <p className="recepie-description-row" key={i}>{row}</p>)
+    } else {
+        description = <p className="recepie-description-missing"> ~ Recept saknas ~ </p>
+    }
 
     return (
         <div className="recepie-description">
