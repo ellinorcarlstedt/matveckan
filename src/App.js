6152f7cf953888a30d7ac62981c755924b65f2d9
Ellinor Carlstedt <ellinor_carlstedt@msn.com>
2020-09-16 15:21:56 +0200

    Now using sass live compiler in vs code instead of scout

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {
   Route,
   Link
 } from "react-router-dom";
-import './Components/css/App.css';
+import './Components/sass/App.css';
 import MenuModerator from './Components/MenuModerator';
 import RecipeInputModerator from './Components/RecipeInputModerator';
 
