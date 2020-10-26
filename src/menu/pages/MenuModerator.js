2674d1ea1c4cebc54e826fbdaafb3c00e184a366
Ellinor Carlstedt <ellinor_carlstedt@msn.com>
2020-10-26 14:36:26 +0100

    added error-handling in menu-moderator

--- src/menu/pages/MenuModerator.js
+++ src/menu/pages/MenuModerator.js
@@ -1,17 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import { useHttpClient } from "../../shared/hooks/http-hook";
-import Background from "../../shared/UIElements/Background";
+import React, { useState, useEffect, useContext } from 'react';
+import { useHttpClient } from '../../shared/hooks/http-hook';
+import { AuthContext } from "../../shared/context/auth-context";
+import Background from '../../shared/UIElements/Background';
 import MenuDisplayer from './MenuDisplayer';
+import Modal from '../../shared/UIElements/Modal';
+import Button from '../../shared/UIElements/Button';
+import LoadingSpinner from '../../shared/UIElements/LoadingSpinner';
 import ArtistAttribute from '../../shared/UIElements/ArtistAttribute';
 
 function MenuModerator () {
+    const auth = useContext(AuthContext);
     const { isLoading, error, sendRequest, clearError } = useHttpClient();
+    const [ noRecipesError, setNoRecipesError ] = useState(false);
     const [ allRecipes, setAllRecipes ] = useState(null);
     const [ selectedRecipes, setSelectedRecipes ] = useState([]);
 
     const reloadMenu = () => {
-        const newMenu = getMenu(allRecipes, 7);
-        setSelectedRecipes(newMenu);
+        if (allRecipes) {
+            const newMenu = getMenu(allRecipes, 7);
+            setSelectedRecipes(newMenu);
+        } else {
+            setNoRecipesError(true);
+        }
     }
 
     const getRandomNumber = (maxNumber) => {
@@ -44,10 +54,28 @@ function MenuModerator () {
     let menu = selectedRecipes;
 
     return (
-    <Background className="menu-moderator">
-        <MenuDisplayer menu={menu} reloadMenu={() => reloadMenu()}/> 
-        <ArtistAttribute />
-    </Background>
+        <React.Fragment>
+            <Modal 
+                show={!!error}
+                onCancel={clearError}
+                header="Någonting gick fel"
+                footer={<Button onClick={clearError}>OK</Button>}>
+                {error}
+            </Modal>
+            <Modal 
+                show={noRecipesError}
+                onCancel={() => setNoRecipesError(false)}
+                header="Kan inte visa några recept"
+                footer={<Button onClick={() => setNoRecipesError(false)}>OK</Button>}>
+                {!auth.isLoggedin && "Du verkar inte vara inloggad. Logga in eller bli medlem för att lägga till och se recept."}
+                {auth.isLoggedin && "Du verkar ännu inte ha några sparade recept. Lägg till några!"}
+            </Modal>
+            <Background className="menu-moderator">
+                {isLoading && <LoadingSpinner asOverlay />}
+                <MenuDisplayer menu={menu} reloadMenu={() => reloadMenu()}/> 
+                <ArtistAttribute />
+            </Background>
+        </React.Fragment>
     )
 
 }
