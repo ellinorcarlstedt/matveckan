5053d6d281462381981992642c44fe11d360bac6
Ellinor Carlstedt <ellinor_carlstedt@msn.com>
2020-11-24 16:29:48 +0100

    Re-styled nav-buttons, responsive background, artist-attribute styling

--- src/shared/UIElements/ArtistAttribute.js
+++ src/shared/UIElements/ArtistAttribute.js
@@ -3,7 +3,7 @@ import React from 'react';
 function ArtistAttribute() {
     return (
         <div className="artist-attribute">
-           Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> 
+           <span>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></span> 
         </div>
     )
 }
